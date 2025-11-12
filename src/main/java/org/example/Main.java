42f608282255e7bacec377da516621c3c32d230e
leeeydia <leeeydia@gmail.com>
2025-11-13 02:28:37 +0900

    11.12 과제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,16 +2,37 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+// 문제 : 할인 대상인지 아닌지 출력해주세요.
+// 조건 : 나이가 19세 이하이거나 60세 이상이면 할인 대상입니다.
+// 조건 : 출력예시 처럼 출력되어야 합니다.
+// 조건 : `구현시작` 부분만 수정 할 수 있습니다.
+// 조건 : 4가지 이상의 방법으로 풀어야 합니다.
+// 조건 : 그 중 2가지 방법은 `&&, ||`없이 풀어야 합니다.
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+class Main {
+    public static void main(String[] args) {
+        int age = 43; // 이 값을 바꿔가면서 실행해보세요.
+
+        System.out.println("당신의 나이는 " + age + "살 입니다.");
+
+        // 구현시작
+        // || 사용 (둘 중 하나라도 참이면 참)
+        if(age<=19 || age >=60){
+            System.out.println("할인대상입니다.");
+        }else{
+            System.out.println("할인대상이 아닙니다.");
         }
+        // && 사용 (둘 다 참이어야 함, 둘 다 참인 할인대상 아닌 것의 ! 사용)
+        if(!(age>19&& age<60)){
+            System.out.println(("할인대상입니다"));
+        }else{
+            System.out.println(("할인대상이 아닙니다."));
+        }
+
+        if((age-19)*(age-60)>=0){
+            System.out.println(("할인대상입니다"));
+        }else{System.out.println(("할인대상이 아닙니다."));
+        }
+
     }
 }
