0463c3eeb2d0a4549499cbe86eaa8ef1c59ad449
leeeydia <leeeydia@gmail.com>
2025-11-28 03:06:31 +0900

    11.28 *찍기 연습 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,27 @@
 package org.example;
 
-class Main{
+class Main {
     public static void main(String[] args) {
-        int n=5;
-        for(
-                int i=1; i<=n; i++
-        ){
-            //공백
-            for(int j=1; j<=n-i; j++){
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (
+                    int j = 1; j <= n - i; j++
+            ) {
                 System.out.print(" ");
             }
-            for(int j=1; j<=i;j++){
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n; i >= 1; i--) {
+            for (
+                    int j = 1; j <= n - i; j++
+            ) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
