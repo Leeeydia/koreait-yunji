df24142f73a50062a09a0fd7d507f077b521942c
leeeydia <leeeydia@gmail.com>
2025-11-28 02:57:19 +0900

    11.28 *찍기 연습 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,16 @@ class Main{
     public static void main(String[] args) {
         int n=5;
         for(
-                int i=1; i<=5; i++
+                int i=1; i<=n; i++
         ){
+            //공백
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
             for(int j=1; j<=i;j++){
                 System.out.print("*");
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 }
